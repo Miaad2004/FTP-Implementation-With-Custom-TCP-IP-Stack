0a56adb0c823975ce078b36d05a0abd85983649d
Miaad <Miaad@MIAAD>
2024-11-24 23:50:04 +0330

    add utility functions for IP address conversion and checksum calculation

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,3 +1,4 @@
 from .utils import mac_addr_to_bytes, mac_addr_to_str
 
-__all__ = ["mac_addr_to_bytes", "mac_addr_to_str"]
+__all__ = ["mac_addr_to_bytes", "mac_addr_to_str",
+           "ip_to_bytes", "ip_to_str", "calculate_checksum"]
